4dab7ec01423d2dc5e11c25ec648f31e670442d5
Rifadh KP <rifad.kp@marketsimplified.com>
2023-05-18 12:20:07 +0530

    Initial commit

---
+++ App.tsx
@@ -0,0 +1,37 @@
+import React from "react";
+import { Button, SafeAreaView, TextInput, View } from "react-native";
+
+const App = () => {
+
+    const onChangeText = (text: string) => {
+        console.log("Input Value", text)
+    };
+    return (
+        <View style={{ flex: 1, backgroundColor: 'white' }}>
+            <View style={{
+                flex: 1,
+                justifyContent: 'center',
+                alignItems: 'center',
+                backgroundColor: 'white'
+            }}>
+                <TextInput
+                    style={{
+                        width: 150,
+                        height: 50,
+                        borderWidth: 1
+                    }}
+                    placeholder="Hai"
+                    onChangeText={onChangeText}
+                />
+            </View>
+            <SafeAreaView >
+                <Button
+                    title="Press me"
+                    onPress={() => { }}
+                />
+            </SafeAreaView>
+        </View>
+    )
+}
+
+export default App;
